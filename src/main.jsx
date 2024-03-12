c7bbd21da5c72f54d7f413a90c63929b35d8da09
Karan Sharma <106376841+karanlakhanpal@users.noreply.github.com>
2024-03-12 23:10:21 +0530

    Done changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import SignIn from "./components/Auth/SignIn.jsx";
+import SignUp from "./components/Auth/SignUp.jsx";
+import Dashboard from "./components/Task/Dashboard.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
