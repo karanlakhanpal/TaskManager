cd36ccb4615ed0efce0dad64058ad6565ecf20aa
anoopmlk <anoopdhiman.mlk@gmail.com>
2024-04-16 17:25:27 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "./components/Common/NavBar";
 import Dashboard from "./components/Task/Dashboard";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Outlet />
     </div>
   );
