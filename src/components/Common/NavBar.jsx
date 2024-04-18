38519151e2488699b3be0bfce9c8280511e0cadb
Karan Sharma <106376841+karanlakhanpal@users.noreply.github.com>
2024-04-18 22:40:04 +0530

    changes

--- src/components/Common/NavBar.jsx
+++ src/components/Common/NavBar.jsx
@@ -1,80 +1,82 @@
-import React, { useState } from 'react'
-import image from '../../assets/karan.jpg'
+import React, { useState } from "react";
+import image from "../../assets/karan.jpg";
 import { MdLogout, MdMenu } from "react-icons/md";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    return (
-        // <div className="bg-black bg-opacity-80 h-20 p-5 flex flex-row gap-x-3 justify-between">
-        //     <div className="flex  items-center">
-        //         <h1 className="text-2xl font-bold text-white">Dashboard</h1>
-        //     </div>
-        //     <div className="flex flex-row gap-x-5">
-        //         <div className="flex  items-center">
-        //             <ul className='flex flex-row gap-x-5'>
-        //                 <li className='text-white hover:underline'><Link to={'#'}>Dashboard</Link></li>
-        //                 <li className='text-white hover:underline'><Link to={'#'}>Tasks</Link></li>
-        //                 <li className='text-white hover:underline'><Link to={'#'}>Profile</Link></li>
-        //                 <li className='text-white hover:underline'><Link to={'#'}>Settings</Link></li>
-        //             </ul>
-        //         </div>
-        //         <div className="flex items-center">
-        //             <img
-        //                 src={image}
-        //                 alt="user logo"
-        //                 height={50}
-        //                 width={50}
-        //                 className="rounded-full"
-        //             />
-        //         </div>
-        //         <div className="flex items-center gap-x-2 border border-1 p-2 rounded border-white">
-        //             <MdLogout color="white" size={20} />
-        //             <p className='text-white'>Logout</p>
-        //         </div>
-        //     </div>
-        // </div>
-        <div className="bg-black bg-opacity-80 p-3 sm:p-5">
-            <div className="flex items-center justify-between">
-                <h1 className="text-xl sm:text-2xl font-bold text-white">TaskManager</h1>
-                <button onClick={() => setMenuOpen(!menuOpen)} className="sm:hidden">
-                    <MdMenu color='white' size={30} />
-                </button>
-                <div className={`flex flex-row hidden sm:flex `}>
-                    <ul className='mt-2 sm:mt-0 flex sm:flex-row sm:gap-x-5'>
-                        <li className="text-white hover:underline mb-2 sm:mb-0">
-                            <Link to="#">Dashboard</Link>
-                        </li>
-                        <li className="text-white hover:underline mb-2 sm:mb-0">
-                            <Link to="#">Tasks</Link>
-                        </li>
-                        <li className="text-white hover:underline mb-2 sm:mb-0">
-                            <Link to="#">Profile</Link>
-                        </li>
-                        <li className="text-white hover:underline mb-2 sm:mb-0">
-                            <Link to="#">Settings</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className={`flex sm:hidden ${menuOpen ? 'hidden' : 'flex-col'}`}>
-                <ul className='mt-2 flex flex-col'>
-                    <li className="text-white hover:underline mb-2">
-                        <Link to="#">Dashboard</Link>
-                    </li>
-                    <li className="text-white hover:underline mb-2">
-                        <Link to="#">Tasks</Link>
-                    </li>
-                    <li className="text-white hover:underline mb-2">
-                        <Link to="#">Profile</Link>
-                    </li>
-                    <li className="text-white hover:underline mb-2">
-                        <Link to="#">Settings</Link>
-                    </li>
-                </ul>
-            </div>
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    // <div className="bg-black bg-opacity-80 h-20 p-5 flex flex-row gap-x-3 justify-between">
+    //     <div className="flex  items-center">
+    //         <h1 className="text-2xl font-bold text-white">Dashboard</h1>
+    //     </div>
+    //     <div className="flex flex-row gap-x-5">
+    //         <div className="flex  items-center">
+    //             <ul className='flex flex-row gap-x-5'>
+    //                 <li className='text-white hover:underline'><Link to={'#'}>Dashboard</Link></li>
+    //                 <li className='text-white hover:underline'><Link to={'#'}>Tasks</Link></li>
+    //                 <li className='text-white hover:underline'><Link to={'#'}>Profile</Link></li>
+    //                 <li className='text-white hover:underline'><Link to={'#'}>Settings</Link></li>
+    //             </ul>
+    //         </div>
+    //         <div className="flex items-center">
+    //             <img
+    //                 src={image}
+    //                 alt="user logo"
+    //                 height={50}
+    //                 width={50}
+    //                 className="rounded-full"
+    //             />
+    //         </div>
+    //         <div className="flex items-center gap-x-2 border border-1 p-2 rounded border-white">
+    //             <MdLogout color="white" size={20} />
+    //             <p className='text-white'>Logout</p>
+    //         </div>
+    //     </div>
+    // </div>
+    <div className="bg-black bg-opacity-80 p-3 sm:p-5">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl sm:text-2xl font-bold text-white">
+          TaskManager
+        </h1>
+        <button onClick={() => setMenuOpen(!menuOpen)} className="sm:hidden">
+          <MdMenu color="white" size={30} />
+        </button>
+        <div className={`flex-row hidden sm:flex`}>
+          <ul className="sm:mt-0 flex sm:flex-row sm:gap-x-5">
+            <li className="text-white hover:underline mb-2 sm:mb-0">
+              <Link to="#">Dashboard</Link>
+            </li>
+            <li className="text-white hover:underline mb-2 sm:mb-0">
+              <Link to="#">Tasks</Link>
+            </li>
+            <li className="text-white hover:underline mb-2 sm:mb-0">
+              <Link to="#">Profile</Link>
+            </li>
+            <li className="text-white hover:underline mb-2 sm:mb-0">
+              <Link to="#">Settings</Link>
+            </li>
+          </ul>
         </div>
-    )
-}
+      </div>
+      <div className={`flex ${!menuOpen && "hidden"} sm:hidden`}>
+        <ul className="mt-4 flex flex-col">
+          <li className="text-white hover:underline mb-2">
+            <Link to="#">Dashboard</Link>
+          </li>
+          <li className="text-white hover:underline mb-2">
+            <Link to="#">Tasks</Link>
+          </li>
+          <li className="text-white hover:underline mb-2">
+            <Link to="#">Profile</Link>
+          </li>
+          <li className="text-white hover:underline mb-2">
+            <Link to="#">Settings</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/Task/Dashboard.jsx
+++ src/components/Task/Dashboard.jsx
@@ -3,38 +3,48 @@ import image from "../../assets/karan.jpg";
 import ReactModal from "react-modal";
 import Task from "../../Services/Task";
 import TaskForm from "./TaskForm";
-import { MdLogout } from "react-icons/md";
+import {
+  MdLogout,
+  MdTask,
+  MdList,
+  MdCheckCircle,
+  MdPending,
+} from "react-icons/md";
 
 const Dashboard = () => {
   const [show, setShow] = useState(false);
   const [showForm, setShowForm] = useState(false);
 
   return (
-    <div className="flex flex-col min-h-screen bg-white">
+    <div className="flex flex-col bg-white">
       {/* Tabs */}
-      <div className="sm:flex sm:flex-row sm:flex-wrap p-2 gap-2">
-        <div className="w-full sm:w-[49.5%] bg-[#2980B9] h-14 p-2 rounded">
-          <p className="text-white">All Tasks</p>
-          <p className="text-white">0</p>
+      <div className="flex flex-col sm:flex sm:flex-row sm:flex-wrap p-2 gap-2">
+        <div className="w-full sm:w-[49.4%] bg-[#2980B9] p-3 h-auto rounded-md">
+          <MdTask color="white" size={25} />
+          <p className="text-white mt-1">All Tasks</p>
+          <p className="text-white sm:text-xl">0</p>
         </div>
-        <div className="w-full sm:w-[49.5%] bg-[#2980B9] h-14 p-2 rounded">
-          <p className="text-white">Active Tasks</p>
-          <p className="text-white">0</p>
+        <div className="w-full sm:w-[49.4%] bg-[#2980B9] p-3 h-auto rounded-md">
+          <MdList color="white" size={25} />
+          <p className="text-white mt-1">Active Tasks</p>
+          <p className="text-white sm:text-xl">0</p>
         </div>
-        <div className="w-full sm:w-[49.5%] bg-[#2980B9] h-14 p-2 rounded">
-          <p className="text-white">Completed Tasks</p>
-          <p className="text-white">0</p>
+        <div className="w-full sm:w-[49.4%] bg-[#2980B9] p-3 h-auto rounded-md">
+          <MdCheckCircle color="white" size={25} />
+          <p className="text-white mt-1">Completed Tasks</p>
+          <p className="text-white sm:text-xl">0</p>
         </div>
-        <div className="w-full sm:w-[49.5%] bg-[#2980B9] h-14 p-2 rounded">
-          <p className="text-white">Due Tasks</p>
-          <p className="text-white">0</p>
+        <div className="w-full sm:w-[49.4%] bg-[#2980B9] p-3 h-auto rounded-md">
+          <MdPending color="white" size={25} />
+          <p className="text-white mt-1">Due Tasks</p>
+          <p className="text-white sm:text-xl">0</p>
         </div>
       </div>
       <button
         onClick={() => setShow(true)}
-        className="bg-white border border-white absolute bottom-6 right-6 text-lg text-black px-6 py-2 rounded-xl hover:text-white hover:bg-transparent hover:border"
+        className="bg-[#2980B9] absolute bottom-3 right-3 text-lg text-white px-6 py-2 rounded-xl"
       >
-        Add Task +
+        +
       </button>
       <ReactModal
         isOpen={show}
